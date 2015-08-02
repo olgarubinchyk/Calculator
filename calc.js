818687defda04706aefddb488e4616e4206006a9
olgarubinchyk <gabka@ua.fm>
2015-08-02 18:39:30 +0300

    Create calc.js

---
+++ calc.js
@@ -0,0 +1,12 @@
+function reset() { //Reset function - if click on "C" button
+	
+	disp.value = "";
+	
+}
+function clickN(num) { //Reset function - if click on "C" button
+	/*var num = k8.value;*/
+	if (disp.value.length<12) {  
+			disp.value = disp.value + num;
+	}
+	else {}
+}
